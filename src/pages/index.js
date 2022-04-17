8283acf3dc51e536f8cf212e41436a0156f22b6c
GuilhermeJordao <grjs4002@gmail.com>
2022-04-17 18:05:43 -0300

    Update Header LunaWebsite

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -31,12 +31,67 @@ export default function App() {
         modules={[Keyboard, Pagination, Navigation]}
         className="mySwiper"
       >
-      <SwiperSlide><img src='https://i.imgur.com/o3qrlo6.png' className="Banner01"/> </SwiperSlide>
-      <SwiperSlide><img src='https://i.imgur.com/TGKw3ks.png' width='100%'/></SwiperSlide>
-      <SwiperSlide><img src='https://i.imgur.com/fquAXXd.png' width='100%'/></SwiperSlide>
-      <SwiperSlide><img src='https://i.imgur.com/PLj1tmZ.png' width='100%'/></SwiperSlide>
-      <SwiperSlide><img src='https://i.imgur.com/gA1vZCY.png' width='100%'/></SwiperSlide>
-    </Swiper>
+        <SwiperSlide>
+          <div style={{ position: 'relative' }}>
+            <div data-background="..." class="with-gradient">
+              <p className="TextoBanner1">
+                <img src='https://i.imgur.com/tLftPOF.png' className="Logo01" alt="Logo"/>
+                <br />
+              </p>
+              <img src='https://i.imgur.com/gLnMbcS.png' className="Banner01" alt="BannerMangá" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div style={{ position: 'relative' }}>
+            <div data-background="..." class="with-gradient">
+              <p className="TextoBanner1">
+                <img src='https://i.imgur.com/IHXS8Sx.png' className="Logo02" alt="Logo"/>
+                <br />
+              </p>
+              <img src='https://i.imgur.com/hhCDejR.png' className="Banner02" alt="BannerMangá" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div style={{ position: 'relative' }}>
+            <div data-background="..." class="with-gradient">
+              <p className="TextoBanner1">
+                <img src='https://i.imgur.com/RelyKXe.png' className="Logo03" alt="Logo" />
+                <br />
+              </p>
+              <img src='https://i.imgur.com/gxvo6w8.png' className="Banner03" alt="BannerMangá" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div style={{ position: 'relative' }}>
+            <div data-background="..." class="with-gradient">
+              <p className="TextoBanner1">
+                <img src='https://i.imgur.com/SljvjLx.png' className="Logo04" alt="Logo"/>
+                <br />
+              </p>
+              <img src='https://i.imgur.com/WaUt3Bn.png' className="Banner04" alt="BannerMangá" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div style={{ position: 'relative' }}>
+            <div data-background="..." class="with-gradient">
+              <p className="TextoBanner1">
+                <img src='https://i.imgur.com/smVV9TQ.png' className="Logo05" alt="Logo"/>
+                <br />
+              </p>
+              <img src='https://i.imgur.com/gf0Bs50.png' className="Banner05" alt="BannerMangá" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+      </Swiper>
     </>
   );
 }
